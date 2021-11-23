520a097e2a32a0c766898498619c3e90e8827123
vbyrnes <BYRNESVA@GMAIL.COM>
2021-11-23 02:29:50 -0500

    Client and server

---
+++ client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import socket
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostbyname()
+
+port=444
+
+clientsocket.connect((host, port))
+
+message = clientsocket.recv(1024)
+
+clientsocket.close()
+
+print(message.decode('ascii'))
+
---
+++ server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostbyname()
+port = 444
+
+serversocket.bind((host, port))
+
+serversocket.listen(3)
+
+while True:
+    clientsocket, address = serversocket.accept()
+
+    print("received connection from " % str(address))
+
+    message = 'Hello! Thank you for connecting to the server.' + "\r\n"
+    
+    clientsocket.send(message.encode('ascii'))
+
+    clientsocket.close()
